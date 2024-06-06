95cf05eb894e6ea4eafc3e5a9b6c41f99170af22
Phumrapee Limpianchop <git@rayriffy.com>
2024-06-07 02:17:51 +0900

    refactor: restore pwa capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,71 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "SUSHIRO",
+        short_name: "SUSHIRO",
+        description: "SUSHIRO calculator",
+        categories: ["utilities", "food"],
+        theme_color: "#fff",
+        background_color: "#fff",
+        display: "standalone",
+        orientation: "portrait",
+        id: "/",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            purpose: "maskable",
+            src: "/assets/icons/manifest-128-maskable.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            purpose: "maskable",
+            src: "/assets/icons/manifest-192-maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            purpose: "maskable",
+            src: "/assets/icons/manifest-512-maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/assets/icons/manifest-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/assets/icons/manifest-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/assets/icons/manifest-180.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            src: "/assets/icons/manifest-128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "/assets/icons/manifest-120.png",
+            sizes: "120x120",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
