78741ac5be6df6e38f4f9d7ddf5e93d2ea9a9457
Phumrapee Limpianchop <git@rayriffy.com>
2024-06-07 02:42:34 +0900

    chore: copywriting

--- src/packlets/numpad/index.svelte
+++ src/packlets/numpad/index.svelte
@@ -4,9 +4,9 @@
   import { handlePadInput } from './handlePadInput'
 </script>
 
-<section class="fixed bottom-0 left-0 right-0 space-y-6 max-w-sm mx-auto">
+<section class="fixed bottom-0 left-0 right-0 max-w-sm mx-auto">
   <slot />
-  <ul class="grid grid-cols-3 gap-4 px-6 pb-6">
+  <ul class="grid grid-cols-3 gap-4 px-6 pt-6">
     {#each Object.values(ButtonValue) as value}
       <PadButton
         value={value}
@@ -14,4 +14,7 @@
       />
     {/each}
   </ul>
+  <footer class="pt-4 pb-6 text-gray-400">
+    <p class="text-center text-xs sm:text-sm">&copy; 2024 Phumrapee L. | <a href="https://rayriffy.com" class="underline" target="_blank" rel="noopener noreferrer">Home</a> <a href="https://github.com/rayriffy/sushiro" class="underline" target="_blank" rel="noopener noreferrer">GitHub</a></p>
+  </footer>
 </section>
