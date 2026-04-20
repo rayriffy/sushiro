cb08c1b757db54b21a2af0e659d225d6be9cffe4
Phumrapee Limpianchop <git@rayriffy.com>
2026-04-20 18:54:25 +0700

    chore: deps update

--- src/packlets/numpad/index.svelte
+++ src/packlets/numpad/index.svelte
@@ -15,6 +15,6 @@
     {/each}
   </ul>
   <footer>
-    <p>&copy; 2024 - {new Date().getFullYear()} Phumrapee L. | <a href="https://rayriffy.com" target="_blank" rel="noopener noreferrer">Home</a> <a href="https://github.com/rayriffy/sushiro" target="_blank" rel="noopener noreferrer">GitHub</a></p>
+    <p>&copy; {new Date().getFullYear()} Phumrapee L. | <a href="https://rayriffy.com" target="_blank" rel="noopener noreferrer">Home</a> <a href="https://github.com/rayriffy/sushiro" target="_blank" rel="noopener noreferrer">GitHub</a></p>
   </footer>
 </section>
