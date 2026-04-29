9983b0d174ed08da37e6d53cd53731d7fe75ec35
Phumrapee Limpianchop <git@rayriffy.com>
2026-04-29 23:54:37 +0700

    fix: toLocaleString in some locale use comma as decimal seperator

--- src/packlets/display/main.svelte
+++ src/packlets/display/main.svelte
@@ -3,12 +3,13 @@
   import { inventoryAtom } from '../price/inventoryAtom'
   import { chargeAtom } from '../price/chargeAtom'
   import { calculate } from '../price/calculate'
+  import { formatDigitString } from '../price/format'
 </script>
 
 <h1 class="text-7xl font-bold">
   {#if $inputAtom === null}
     {calculate($inventoryAtom, $chargeAtom)}
   {:else}
-    {Number($inputAtom.value).toLocaleString()}
+    {formatDigitString($inputAtom.value)}
   {/if}
 </h1>
--- src/packlets/price/calculate.ts
+++ src/packlets/price/calculate.ts
@@ -1,12 +1,19 @@
+import { formatAmountDisplay } from "./format";
+
+export const calculateRaw = (
+  inventory: Record<number, number>,
+  charge: boolean,
+) => {
+  return (
+    Object.entries(inventory).reduce((acc, [price, stock]) => {
+      return acc + Number(price) * stock;
+    }, 0) * (charge ? 1.1 : 1)
+  );
+};
+
 export const calculate = (
   inventory: Record<number, number>,
   charge: boolean,
 ) => {
-  const total = (
-    Object.entries(inventory).reduce((acc, [price, stock]) => {
-      return acc + Number(price) * stock;
-    }, 0) * (charge ? 1.1 : 1)
-  ).toLocaleString(undefined, { minimumFractionDigits: 2 });
-
-  return total.endsWith(".00") ? total.slice(0, -3) : total;
+  return formatAmountDisplay(calculateRaw(inventory, charge));
 };
